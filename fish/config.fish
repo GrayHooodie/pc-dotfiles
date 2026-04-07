fd2167b309e81a7486fab0396c30ff511408e04d
GrayHoodie <matrala0419@gmail.com>
2026-04-07 10:02:57 -0500

    removed spotify

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -gx RANGER_LOAD_DEFAULT_RC false 
 set -gx GRIM_DEFAULT_DIR ~/Pictures/Grim/
 set TERM xterm-256color
-set -U fish_user_paths /home/matt/opt/REAPER /home/matt/opt/idea-IU-261.20869.38/bin /home/matt/opt/pycharm-261.20869.49/bin /home/matt/opt/RubyMine-261.20869.43/bin /home/matt/opt/RustRover-261.20869.45/bin
+
+# HOW TO JUST ADD MORE PATHS
+#set -U fish_user_paths /home/matt/opt/REAPER /home/matt/opt/idea-IU-261.20869.38/bin /home/matt/opt/pycharm-261.20869.49/bin /home/matt/opt/RubyMine-261.20869.43/bin /home/matt/opt/RustRover-261.20869.45/bin
+
+set -x PATH /home/matt/Scripts:/home/matt/.local/bin:/home/matt/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
